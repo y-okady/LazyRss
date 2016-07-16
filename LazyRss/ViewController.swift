5d3a9eda21ff5e89e31c51e7586d7937af163016
yuki-okada <oka5130@gmail.com>
2016-07-16 15:35:15 +0900

    URL入力欄を追加

--- LazyRss/ViewController.swift
+++ LazyRss/ViewController.swift
@@ -8,16 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var urlField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        urlField.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print(urlField.text)
+        urlField.resignFirstResponder()
+        return true
+    }
+    
+    func showItems(url: String) {
+        
     }
 
 
