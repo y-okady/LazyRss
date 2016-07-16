f0f524b8e4c87dd1596fcfd6dcd66b4f40c23195
yuki-okada <oka5130@gmail.com>
2016-07-16 14:29:27 +0900

    Initial Commit

---
+++ LazyRss/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LazyRss
+//
+//  Created by YUKI OKADA on 2016/07/16.
+//  Copyright © 2016年 YUKI OKADA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
